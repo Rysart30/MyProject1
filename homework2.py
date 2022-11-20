585c8f45f2da1f67a4df94ed19af008429710edc
Rysart30 <rys.artem.30@gmail.com>
2022-11-20 21:25:51 +0200

    commit 2

--- homework2.py
+++ homework2.py
@@ -20,3 +20,20 @@ def retry(attempts=5, desired_value=None):
         return wrapper
 
     return decorator
+
+
+def draw_square(n, s, f):
+    r = s * n
+    if not f:
+        m = s + " " * (n - 2) + s
+    else:
+        m = r
+    print(r)
+    for i in range(n - 2):
+        print(m)
+    print(r)
+
+
+draw_square(5, "#", True)
+print("")
+draw_square(5, "*", False)
